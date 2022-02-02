a86fc5ee1f2ee06ae3ae5bd4d95dce874a9ec580
aswinak799 <94048895+aswinak799@users.noreply.github.com>
2022-02-02 13:43:25 +0530

    Add files via upload

---
+++ Graphics/circle.py
@@ -0,0 +1,8 @@
+import math
+def cirarea(r):
+    
+    area=math.pi*r*r
+    return area
+def cirperi(r):
+    peri=math.pi*2*r
+    return peri
---
+++ Graphics/rectangle.py
@@ -0,0 +1,6 @@
+def rectarea(lng,brd):
+    area=lng*brd
+    return area
+def rectperi(l,b):
+    peri=2*(l+b)
+    return peri
