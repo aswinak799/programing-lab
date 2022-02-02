0b8df3d48e4bed55756b9be7717f972aecb4ddd8
aswinak799 <94048895+aswinak799@users.noreply.github.com>
2022-02-02 13:42:44 +0530

    Add files via upload

---
+++ Graphics/Graphics3D/cuboid.py
@@ -0,0 +1,6 @@
+def cubarea(l,b,h):
+    a=2*((l*b)+(l*h)+(b*h))
+    return a
+def cubper(l,b,h):
+    p=4*(l+b+h)
+    return p
---
+++ Graphics/Graphics3D/sphere.py
@@ -0,0 +1,7 @@
+import math
+def spharea(r):
+    area=math.pi*r*r*4
+    return area
+def scircum(r):
+    c=2*math.pi*r
+    return c
